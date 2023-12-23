f95e3bd614362187b1494673ccf31ae41405d073
Medhat <matouk7@outlook.com>
2023-12-23 03:09:11 +0200

    Basic Auth

--- bookProject/account/views.py
+++ bookProject/account/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.contrib.auth.models import User, Group
+from .serializers import UserSerializer, GroupSerializer
+from rest_framework import  viewsets
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAuthenticated]
+
